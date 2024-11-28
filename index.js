b2c767d159223c14b7d7b7c14f6eb8099701cdd7
DiChita <DinupSachita@dichitas-mbp.mynetworksettings.com>
2024-11-28 09:23:16 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,233 @@
+document.getElementById('gmailButton').addEventListener('click', function() {
+  window.open('https://www.gmail.com', '_blank');
+});
+
+document.getElementById('youtubeButton').addEventListener('click', function() {
+    window.open('https://www.youtube.com', '_blank');
+  });
+
+
+document.getElementById('facebookButton').addEventListener('click', function() {
+    window.open('https://www.facebook.com', '_blank');
+  });
+
+  document.getElementById('instagramButton').addEventListener('click', function() {
+    window.open('https://www.instagram.com', '_blank');
+  });
+
+  document.getElementById('twitterButton').addEventListener('click', function() {
+    window.open('https://www.twitter.com', '_blank');
+  });
+
+
+  document.getElementById('linkedinButton').addEventListener('click', function() {
+    window.open('https://www.linkedin.com', '_blank');
+  });
+  
+  document.getElementById('pinterestButton').addEventListener('click', function() {
+    window.open('https://www.pinterest.com', '_blank');
+  });
+  
+  document.getElementById('tumblrButton').addEventListener('click', function() {
+    window.open('https://www.tumblr.com', '_blank');
+  });
+  
+  document.getElementById('redditButton').addEventListener('click', function() {
+    window.open('https://www.reddit.com', '_blank');
+  });
+  
+  document.getElementById('githubButton').addEventListener('click', function() {
+    window.open('https://www.github.com', '_blank');
+  });
+  
+  document.getElementById('stackoverflowButton').addEventListener('click', function() {
+    window.open('https://www.stackoverflow.com', '_blank');
+  });
+  
+  document.getElementById('mediumButton').addEventListener('click', function() {
+    window.open('https://www.medium.com', '_blank');
+  });
+
+
+
+// Add more listeners for other popular websites
+document.getElementById('amazonButton').addEventListener('click', function() {
+    window.open('https://www.amazon.com', '_blank');
+  });
+  
+  document.getElementById('wikipediaButton').addEventListener('click', function() {
+    window.open('https://www.wikipedia.org', '_blank');
+  });
+  
+  document.getElementById('googleButton').addEventListener('click', function() {
+    window.open('https://www.google.com', '_blank');
+  });
+  
+  document.getElementById('netflixButton').addEventListener('click', function() {
+    window.open('https://www.netflix.com', '_blank');
+  });
+  
+  document.getElementById('appleButton').addEventListener('click', function() {
+    window.open('https://www.apple.com', '_blank');
+  });
+  
+  document.getElementById('spotifyButton').addEventListener('click', function() {
+    window.open('https://www.spotify.com', '_blank');
+  });
+  
+  document.getElementById('cnnButton').addEventListener('click', function() {
+    window.open('https://www.cnn.com', '_blank');
+  });
+  
+  document.getElementById('bbcButton').addEventListener('click', function() {
+    window.open('https://www.bbc.com', '_blank');
+  });
+  
+  document.getElementById('etsyButton').addEventListener('click', function() {
+    window.open('https://www.etsy.com', '_blank');
+  });
+  
+  document.getElementById('paypalButton').addEventListener('click', function() {
+    window.open('https://www.paypal.com', '_blank');
+  });
+  
+  document.getElementById('dropboxButton').addEventListener('click', function() {
+    window.open('https://www.dropbox.com', '_blank');
+  });
+  
+  document.getElementById('yahooButton').addEventListener('click', function() {
+    window.open('https://www.yahoo.com', '_blank');
+  });
+  
+  document.getElementById('zoomButton').addEventListener('click', function() {
+    window.open('https://www.zoom.us', '_blank');
+  });
+  
+  document.getElementById('airbnbButton').addEventListener('click', function() {
+    window.open('https://www.airbnb.com', '_blank');
+  });
+  
+  document.getElementById('vimeoButton').addEventListener('click', function() {
+    window.open('https://www.vimeo.com', '_blank');
+  });
+  
+  document.getElementById('snapchatButton').addEventListener('click', function() {
+    window.open('https://www.snapchat.com', '_blank');
+  });
+  
+  document.getElementById('tiktokButton').addEventListener('click', function() {
+    window.open('https://www.tiktok.com', '_blank');
+  });
+  
+  document.getElementById('huluButton').addEventListener('click', function() {
+    window.open('https://www.hulu.com', '_blank');
+  });
+  
+  document.getElementById('flickrButton').addEventListener('click', function() {
+    window.open('https://www.flickr.com', '_blank');
+  });
+  
+  document.getElementById('salesforceButton').addEventListener('click', function() {
+    window.open('https://www.salesforce.com', '_blank');
+  });
+  
+  document.getElementById('quoraButton').addEventListener('click', function() {
+    window.open('https://www.quora.com', '_blank');
+  });
+  
+  document.getElementById('walmartButton').addEventListener('click', function() {
+    window.open('https://www.walmart.com', '_blank');
+  });
+  
+  document.getElementById('bestbuyButton').addEventListener('click', function() {
+    window.open('https://www.bestbuy.com', '_blank');
+  });
+  
+  document.getElementById('targetButton').addEventListener('click', function() {
+    window.open('https://www.target.com', '_blank');
+  });
+  
+  document.getElementById('nikeButton').addEventListener('click', function() {
+    window.open('https://www.nike.com', '_blank');
+  });
+  
+  document.getElementById('redditButton').addEventListener('click', function() {
+    window.open('https://www.reddit.com', '_blank');
+  });
+  
+  document.getElementById('bbcButton').addEventListener('click', function() {
+    window.open('https://www.bbc.com', '_blank');
+  });
+  
+  document.getElementById('wordpressButton').addEventListener('click', function() {
+    window.open('https://www.wordpress.com', '_blank');
+  });
+  
+  document.getElementById('stackoverflowButton').addEventListener('click', function() {
+    window.open('https://www.stackoverflow.com', '_blank');
+  });
+
+
+  document.getElementById('indeedButton').addEventListener('click', function() {
+    window.open('https://www.indeed.com', '_blank');
+  });
+  
+  document.getElementById('monsterButton').addEventListener('click', function() {
+    window.open('https://www.monster.com', '_blank');
+  });
+  
+  document.getElementById('glassdoorButton').addEventListener('click', function() {
+    window.open('https://www.glassdoor.com', '_blank');
+  });
+  
+  document.getElementById('leverButton').addEventListener('click', function() {
+    window.open('https://www.lever.co', '_blank');
+  });
+  
+  document.getElementById('ziprecruiterButton').addEventListener('click', function() {
+    window.open('https://www.ziprecruiter.com', '_blank');
+  });
+  
+  document.getElementById('careerbuilderButton').addEventListener('click', function() {
+    window.open('https://www.careerbuilder.com', '_blank');
+  });
+  
+  document.getElementById('workableButton').addEventListener('click', function() {
+    window.open('https://www.workable.com', '_blank');
+  });
+  
+  document.getElementById('linkedinButton').addEventListener('click', function() {
+    window.open('https://www.linkedin.com/jobs', '_blank');
+  });
+  
+  document.getElementById('diceButton').addEventListener('click', function() {
+    window.open('https://www.dice.com', '_blank');
+  });
+  
+  document.getElementById('simplyhiredButton').addEventListener('click', function() {
+    window.open('https://www.simplyhired.com', '_blank');
+  });
+  
+  document.getElementById('jobserveButton').addEventListener('click', function() {
+    window.open('https://www.jobserve.com', '_blank');
+  });
+  
+  document.getElementById('snagajobButton').addEventListener('click', function() {
+    window.open('https://www.snagajob.com', '_blank');
+  });
+  
+  document.getElementById('careerjetButton').addEventListener('click', function() {
+    window.open('https://www.careerjet.com', '_blank');
+  });
+  
+  document.getElementById('jobheroButton').addEventListener('click', function() {
+    window.open('https://www.jobhero.com', '_blank');
+  });
+  
+  document.getElementById('jobhopButton').addEventListener('click', function() {
+    window.open('https://www.jobhop.com', '_blank');
+  });
+  
+  document.getElementById('flexjobsButton').addEventListener('click', function() {
+    window.open('https://www.flexjobs.com', '_blank');
+  });
